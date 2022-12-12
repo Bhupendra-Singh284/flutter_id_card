9f774076ae14255ba32e779d1eb1c2637b64bfeb
ra-jain <rakxhit@gmail.com>
2022-12-12 18:58:31 +0530

    Fixed and Cleaned Code a Bit Use PageView

--- lib/view/components/page_layout.dart
+++ lib/view/components/page_layout.dart
@@ -1,28 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:id_card/view/components/icon_container.dart';
-import 'package:id_card/view/home.dart';
 
-class Pagelayout extends StatefulWidget {
+class Pagelayout extends StatelessWidget {
   final String name;
   final String nickname;
   final String desc;
   final String imagename;
   final String rating;
-  final bool _cval = togglevalue();
+  final bool cval;
 
-  Pagelayout(
+  const Pagelayout(
       {super.key,
       required this.desc,
       required this.imagename,
       required this.name,
       required this.nickname,
-      required this.rating});
-
-  @override
-  State<Pagelayout> createState() => _PagelayoutState();
-}
-
-class _PagelayoutState extends State<Pagelayout> {
+      required this.rating,
+      required this.cval});
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -39,7 +33,7 @@ class _PagelayoutState extends State<Pagelayout> {
               width: 1,
             )),
             image: DecorationImage(
-              image: AssetImage(widget.imagename),
+              image: AssetImage(imagename),
               fit: BoxFit.cover,
             ),
           ),
@@ -47,7 +41,7 @@ class _PagelayoutState extends State<Pagelayout> {
         Padding(
           padding: const EdgeInsets.all(15),
           child: Text(
-            widget.name,
+            name,
             style: const TextStyle(
               fontSize: 20,
               fontWeight: FontWeight.bold,
@@ -59,27 +53,24 @@ class _PagelayoutState extends State<Pagelayout> {
           decoration: BoxDecoration(
               border: Border(
                   bottom: BorderSide(
-                      color: widget._cval ? Colors.amber : Colors.black,
-                      width: 1.8))),
+                      color: cval ? Colors.amber : Colors.black, width: 1.8))),
           child: Padding(
             padding: const EdgeInsets.symmetric(horizontal: 20),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
-                Expanded(
-                  child: Text(
-                    widget.nickname,
-                    textDirection: TextDirection.ltr,
-                    style: const TextStyle(fontSize: 16),
-                  ),
+                Text(
+                  nickname,
+                  textDirection: TextDirection.ltr,
+                  style: const TextStyle(fontSize: 16),
                 ),
                 Row(
                   children: [
                     Icon(
                       Icons.star,
-                      color: widget._cval ? Colors.orange : Colors.red,
+                      color: cval ? Colors.orange : Colors.red,
                     ),
-                    Text(widget.rating)
+                    Text(rating)
                   ],
                 ),
               ],
@@ -101,7 +92,7 @@ class _PagelayoutState extends State<Pagelayout> {
         Padding(
           padding: const EdgeInsets.symmetric(horizontal: 40),
           child: Text(
-            widget.desc,
+            desc,
             style: const TextStyle(
                 fontSize: 18,
                 fontWeight: FontWeight.bold,
--- lib/view/components/third_page.dart
+++ lib/view/components/third_page.dart
@@ -1,36 +1,26 @@
 import 'package:flutter/material.dart';
-import 'package:id_card/view/components/page_container.dart';
-import 'package:id_card/view/home.dart';
-import 'package:id_card/view/second_page.dart';
-import 'package:page_transition/page_transition.dart';
+import 'package:id_card/view/components/page_layout.dart';
 
-class ThirdRoute extends StatelessWidget {
-  const ThirdRoute({super.key});
+class ThirdPage extends StatelessWidget {
+  final bool toggle;
+  const ThirdPage({super.key, required this.toggle});
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onVerticalDragUpdate: (details) {},
-      onPanUpdate: (details) {
-        if (details.delta.dx > 0) {
-          Navigator.of(context).push(PageTransition(
-              child: const SecondRoute(),
-              childCurrent: this,
-              type: PageTransitionType.leftToRightJoined,
-              duration: const Duration(milliseconds: 550)));
-        }
-      },
-      child: Scaffold(
-        backgroundColor: togglevalue() ? Colors.black : Colors.white,
-        body: DefaultTextStyle(
-          style: TextStyle(color: togglevalue() ? Colors.white : Colors.black),
+    return Scaffold(
+      backgroundColor: toggle ? Colors.black : Colors.white,
+      body: SingleChildScrollView(
+        child: DefaultTextStyle(
+          style: TextStyle(color: toggle ? Colors.white : Colors.black),
           child: Pagelayout(
-              desc:
-                  "${'"'}Jiraiya was one of Konohagakure's Sannin. Famed as a hermit and pervert of stupendous ninja skill, Jiraiya travelled the world in search of knowledge that would help his friends, the various novels he wrote, and, posthumously, the world in its entirety – knowledge that would be passed on to his godson and final student, Naruto Uzumaki.${'"'}",
-              imagename: "images/jiraiya_4.jpg",
-              name: "Jiraiya",
-              nickname: "The Toad Sage, Legendary Sanin",
-              rating: "4.6"),
+            desc:
+                "${'"'}Jiraiya was one of Konohagakure's Sannin. Famed as a hermit and pervert of stupendous ninja skill, Jiraiya travelled the world in search of knowledge that would help his friends, the various novels he wrote, and, posthumously, the world in its entirety – knowledge that would be passed on to his godson and final student, Naruto Uzumaki.${'"'}",
+            imagename: "assets/images/jiraiya_4.jpg",
+            name: "Jiraiya",
+            nickname: "The Toad Sage, Legendary Sanin",
+            rating: "4.6",
+            cval: toggle,
+          ),
         ),
       ),
     );
